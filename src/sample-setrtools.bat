65bd5c63002375a695d5790eda7018fdf2c3b835
John Leonard <john@zglog.com>
2012-11-11 05:30:02 -0500

    Removing macros - trying new setup.

--- src/sample-setrtools.bat
+++
@@ -1,6 +0,0 @@
-@ECHO + Setting environment for RTOOLS
-@SET R_VERSION=2.13.1
-@SET PATH=%PATH%;C:\Program Files\R\R-%R_VERSION%\bin;c:\Rtools\bin;c:\Rtools\perl\bin;c:\Rtools\MinGW\bin;c:\Rtools\MinGW64\bin;
-@SET HOME=%HOMEDRIVE%%HOMEPATH%
-@SET SVN_EDITOR=notepad
-@SET CYGWIN=nodosfilewarning
